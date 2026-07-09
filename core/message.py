f62f20ef431682b408a8f6f3129d87749fbb54d6
Alex <klyaksaoff@gmail.com>
2026-07-10 02:12:01 +0300

    new command

--- core/message.py
+++ core/message.py
@@ -18,18 +18,22 @@ class MyCommands(commands.Component):
         print(f"[{payload.broadcaster.name}] - {payload.chatter.name}: "
               f"{payload.text}")
 
+    @commands.cooldown(rate=1, per=60, key=commands.BucketType.chatter)
     @commands.command(aliases=["привет", "hi", "hello"])
     async def welcome(self, ctx: commands.Context) -> None:
         await ctx.reply(f"Привет {ctx.chatter}!")
 
+    @commands.cooldown(rate=3, per=60, key=commands.BucketType.chatter)
     @commands.command(aliases=["рандом", "случайно"])
     async def random(self, ctx: commands.Context) -> None:
         await ctx.reply(f"{ctx.chatter} {random.randint(0, 100)}!")
 
+    @commands.cooldown(rate=3, per=60, key=commands.BucketType.chatter)
     @commands.command(aliases=["донат", "donats", "донаты", "donat"])
     async def donate(self, ctx: commands.Context) -> None:
         await ctx.reply(f"{ctx.chatter}, donatex.gg/donate/pingvinius")
 
+    @commands.cooldown(rate=3, per=60, key=commands.BucketType.chatter)
     @commands.command(aliases=["server", "сервер", "айпи"])
     async def ip(self, ctx: commands.Context) -> None:
         await ctx.reply(
@@ -37,10 +41,12 @@ class MyCommands(commands.Component):
             f"Выживание - (94.29.33.162:25566)\n"
             f"Столбы - (94.29.33.162:25567)")
 
+    @commands.cooldown(rate=2, per=30, key=commands.BucketType.chatter)
     @commands.command(aliases=["tg", "тг", "тгк", "телеграм"])
     async def telegram(self, ctx: commands.Context) -> None:
         await ctx.reply(f"{ctx.chatter}, t.me/pingvinius_228")
 
+    @commands.cooldown(rate=1, per=60, key=commands.BucketType.chatter)
     @commands.command(aliases=["дс", "disco", "дискорд"])
     async def discord(self, ctx: commands.Context) -> None:
         await ctx.reply(f"{ctx.chatter}, discord.gg/pqZ7BSEhJj")
@@ -49,3 +55,8 @@ class MyCommands(commands.Component):
     @commands.cooldown(rate=1, per=60, key=commands.BucketType.chatter)
     async def pin(self, ctx: commands.Context) -> None:
         await ctx.reply("Наш тгк: t.me/pingvinius228 - Конкурс/Оповещения о стримах!")
+
+    @commands.cooldown(rate=2, per=30, key=commands.BucketType.chatter)
+    @commands.command(aliases=["рб", "rb", "рбник", "друзьярб"])
+    async def roblox_friend(self, ctx: commands.Context) -> None:
+        await ctx.reply(f"{ctx.chatter}, KlyaksaOFF - ник в роблоксе.")
